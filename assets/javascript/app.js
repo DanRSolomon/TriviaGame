7f265c280582e9f1eae61ae18d7e8d2f76a0412e
mccloud76 <dansolomon76@yahoo.com>
2018-10-29 14:51:53 -0700

    basic html, added questions array

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+var triviaQuestions = [{
+    question: "What's the name of the closest planet to the Sun?",
+    choices: ["Earth", "Mercury", "Jupiter", "Venus"],
+    correct: 1
+}, {
+    question: "How many moons does Mars have?",
+    choices: ["One", "Two", "Three", "None"],
+    correct: 1
+}, {
+    question: "Which planet is the largest in the Solar System?",
+    choices: ["Neptune", "Saturn", "Jupiter", "Earth"],
+    correct: 2
+}, {
+    question: "Which planet rotates in the opposite direction as the rest?",
+    choices: ["Earth", "Mercury", "Jupiter", "Venus"],
+    correct: 3
+}];
+
+console.log(triviaQuestions);
