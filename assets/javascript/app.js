e3f218ec2cb667b53576e75285048b05a355c3af
mccloud76 <dansolomon76@yahoo.com>
2018-10-29 15:15:45 -0700

    first question displays with possible answers and radio button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -16,4 +16,19 @@ var triviaQuestions = [{
     correct: 3
 }];
 
-console.log(triviaQuestions);
+var currentQuestion = 0;
+var correctAnswer = 0;
+
+$('#questions').html(parseInt(currentQuestion) + 1 + ". " + triviaQuestions[currentQuestion].question);
+var options = triviaQuestions[currentQuestion].choices;
+var formHtml = " ";
+for (var i = 0; i < options.length; i++) {
+    formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
+      triviaQuestions[currentQuestion].choices[i] + '</label></div>';
+}
+
+$('#answer-choices').html(formHtml);
+$("#option0").prop('checked', true);
+console.log(options);
+
+
