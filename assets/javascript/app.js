d0ffee1e9183e122232245189f2be861fc558712
mccloud76 <dansolomon76@yahoo.com>
2018-10-29 21:37:13 -0700

    asks all questions, gives final points tally, added background image, still needs timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+// $(document).ready(function() {
+
 var triviaQuestions = [{
     question: "What's the name of the closest planet to the Sun?",
     choices: ["Earth", "Mercury", "Jupiter", "Venus"],
@@ -17,18 +19,56 @@ var triviaQuestions = [{
 }];
 
 var currentQuestion = 0;
-var correctAnswer = 0;
+var correctAnswers = 0;
 
-$('#questions').html(parseInt(currentQuestion) + 1 + ". " + triviaQuestions[currentQuestion].question);
-var options = triviaQuestions[currentQuestion].choices;
-var formHtml = " ";
-for (var i = 0; i < options.length; i++) {
-    formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
-      triviaQuestions[currentQuestion].choices[i] + '</label></div>';
+function questionSetup() {
+    $('#questions').html(parseInt(currentQuestion) + 1 + ". " + triviaQuestions[currentQuestion].question);
+    var options = triviaQuestions[currentQuestion].choices;
+    var formHtml = " ";
+    for (var i = 0; i < options.length; i++) {
+        formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
+            triviaQuestions[currentQuestion].choices[i] + '</label></div>';
+    }
+
+    $('#answer-choices').html(formHtml);
+    $("#option").prop('checked', true);
+    console.log(options);
 }
 
-$('#answer-choices').html(formHtml);
-$("#option0").prop('checked', true);
-console.log(options);
+function checkAnswer() {
+    if ($("input[name=option]:checked").val() == triviaQuestions[currentQuestion].correct) {
+        correctAnswers++;
+    };
+};
 
+$(document).ready(function () {
+
+    // $(".jumbotron").hide();
+    // $('#start-button').click(function() {
+    //     $(".jumbotron").fadeIn();
+    //     $(this).hide();
+    // });
+
+    questionSetup();
+
+    $("#next").click(function () {
+        event.preventDefault();
+        checkAnswer();
+        currentQuestion++;
+        if (currentQuestion < triviaQuestions.length) {
+            questionSetup();
+            if (currentQuestion == triviaQuestions.length - 1) {
+                $('#next').html("Submit");
+                $('#next').click(function () {
+                    $(".jumbotron").hide();
+                    $("#result").html("You correctly answered " + correctAnswers + " out of " + currentQuestion + " questions! ").hide();
+                    $("#result").fadeIn(1500);
+                })
+            }
+        }
+    });
+});
+// });
+
+// $("#start_button").click(playGame());
 
